7e9fc2c7fd5b926b17ab489a75a7fcaf1acdcc11
Brendan Ewen <brendanewen@Brendans-Air-2.lan>
2024-04-08 20:34:40 +0100

    build: got project working on my macbook

---
+++ FinanceApp/Migrations/20240403140501_Intital3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceApp.Migrations
+{
+    public partial class Intital3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- FinanceApp/SQL/Procedures/InsertPayment.sql
+++ FinanceApp/SQL/Procedures/InsertPayment.sql
@@ -1,11 +1,2 @@
-CREATE PROCEDURE InsertPayment
-    @PaymentName NVARCHAR(100),
-    @PaymentTotal DECIMAL(18, 2),
-    @PaymentDate NVARCHAR(50),
-    @PaymentFreq NVARCHAR(20),
-    @Email NVARCHAR(256)
-AS
-BEGIN
-    INSERT INTO Payments (PaymentName, PaymentTotal, PaymentDate, PaymentFreq, Email)
-    VALUES (@PaymentName, @PaymentTotal, @PaymentDate, @PaymentFreq, @Email);
-END
+
+drop procedure InsertPayment;
